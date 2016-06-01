8e56c4086a446c803271c948bd551c17feac5f0f
sakthisgit <sakthisaravanan.p@gmail.com>
2016-06-01 17:04:40 +0530

    Python List Modules Added

---
+++ pymodules/print_list.py
@@ -0,0 +1,7 @@
+def print_list(list):
+    """ Print the Python List """
+    for each_item in list:
+        if isinstance(list,list):
+            print_list(list)
+         else:
+             print each_item
