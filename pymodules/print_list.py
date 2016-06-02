4e04b0e3c84cd7e99f209c3878d8eb6df1fc39bd
sakthisgit <sakthisaravanan.p@gmail.com>
2016-06-02 10:47:00 +0530

    Indentation corrected

--- pymodules/print_list.py
+++ pymodules/print_list.py
@@ -1,7 +1,9 @@
-def print_list(list):
+""" This Module prints the List """
+
+def print_list(mylist):
     """ Print the Python List """
-    for each_item in list:
-        if isinstance(list,list):
-            print_list(list)
-         else:
-             print each_item
+    for each_item in mylist:
+        if isinstance(each_item, list):
+            print_list(each_item)
+        else:
+            print each_item
