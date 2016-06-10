c824e3ab5b92260f8cbfc06d4a58158b5687143e
Sakthi Saravanakumar <sakthisaravanan.p@gmail.com>
2016-06-10 22:26:32 +0530

    Python Distro Created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name = 'RecursiveListPrinting',
+        version = '0.0.1',
+        py_modules = ['pymodules/print_list'],
+        author = 'Sakthi Saravanakumar P',
+        author_email = 'sakthisaravanan.p@gmail.com',
+        description = 'A simple printer for nested list',    
+    )
