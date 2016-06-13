e3f1ee0d4928628a34899e39c1c07aee2532c0a4
Sakthi Saravanakumar <sakthisaravanan.p@gmail.com>
2016-06-13 14:36:27 +0530

    Files changed

--- pymodules/files.py
+++ pymodules/files.py
@@ -1,9 +1,18 @@
-data=open('sketch.txt')
+import os
 
-for each_line in data:
-    print "Each line",each_line.find(':')
-    if not each_line.find(':') == -1:
-        (role, line_spoken) = each_line.split(':',1)
-        print "%s said %s " %(role, line_spoken)
+os.chdir('/mnt/workspace/Myworkspace/myproject/pymodules')
 
-data.close()
+try:
+    data=open('sketch.txt')
+
+    for each_line in data:
+        try:
+            if not each_line.find(':') == -1:
+                (role, line_spoken) = each_line.split(':',1)
+                print "%s said %s " %(role, line_spoken)
+        except ValueError:
+            print 'Value has not been processed'
+            
+    data.close()
+except IOError:
+    print 'File Not Found'
