ed7de5869798e074e3066f77521cccbb72683ef9
Sakthi Saravanakumar <sakthisaravanan.p@gmail.com>
2016-06-13 11:43:12 +0530

    Function modified

--- build/lib/pymodules/print_list.py
+++ build/lib/pymodules/print_list.py
@@ -10,3 +10,8 @@ def print_list(mylist):
             print_list(each_item)
         else:
             print(each_item)
+
+
+def printv2(mylist):
+    """ Improved Version of Print list """
+    printf("The V2 Function is under developing")
--- pymodules/print_list.py
+++ pymodules/print_list.py
@@ -3,10 +3,13 @@
 	Description: Print the List
 """
 
-def print_list(mylist):
+def print_list(mylist, level):
     """ Print the Python List """
     for each_item in mylist:
         if isinstance(each_item, list):
-            print_list(each_item)
+            print_list(each_item,level)
         else:
+            for tab_stop in range(level):
+                print("\t", end='')
             print(each_item)
+
