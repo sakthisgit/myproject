d8bce3e8d96896320f87b1624181dae13412a8ea
Sakthi Saravanakumar <sakthisaravanan.p@gmail.com>
2016-06-13 11:45:49 +0530

    default value setted

--- pymodules/print_list.py
+++ pymodules/print_list.py
@@ -3,7 +3,7 @@
 	Description: Print the List
 """
 
-def print_list(mylist, level):
+def print_list(mylist, level=0):
     """ Print the Python List """
     for each_item in mylist:
         if isinstance(each_item, list):
